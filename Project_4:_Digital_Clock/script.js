19c04985e84f90216aa3c9844f282838dedfe735
Raushan Gupta <126952814+raushangupta1425@users.noreply.github.com>
2023-04-03 13:52:10 +0000

    add toggle button

--- Project_4:_Digital_Clock/script.js
+++ Project_4:_Digital_Clock/script.js
@@ -5,7 +5,10 @@ setInterval(() => {
     let  Days = document.getElementById('Days');
     let  Dat = document.getElementById('Dat');
     let  ampm = document.getElementById('ampm');
-    
+    let  text = document.querySelector('.text');
+    let  checkbox = document.querySelector('#check');
+    let am="";
+
         let dateTime = new Date();
         let hours = dateTime.getHours();
         let minutes = dateTime.getMinutes();
@@ -13,19 +16,23 @@ setInterval(() => {
         let dat = dateTime.toDateString().slice(3, );
         let days = dateTime.toLocaleString('default', {
             weekday: "long"});
-    
-        let am = hours >= 12 ? "PM" : "AM";
-        if(hours > 12){
+        if(checkbox.checked){
+            text.innerText= "24hrs";
+        }else{
+            text.innerHTML= "12hrs";
+            am = hours >= 12 ? "PM" : "AM";
+            if(hours > 12){
             hours -= 12;
+            }    
         }
         hours = (hours < 10) ? "0" + hours : hours;
         minutes = (minutes < 10) ? "0" + minutes : minutes;
         seconds = (seconds < 10) ? "0" + seconds : seconds;
-    
+                
         Hours.innerText = hours;
         Minutes.innerText = minutes;    
         Seconds.innerText = seconds;    
         Dat.innerText = dat; 
         Days.innerText = days;    
         ampm.innerText = am;    
-    });
+});
