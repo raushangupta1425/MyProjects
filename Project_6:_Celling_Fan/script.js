353e9d8899fa95d69b2a350a7a0dc3e2cfaf4931
Raushan Gupta <126952814+raushangupta1425@users.noreply.github.com>
2023-06-17 08:21:47 +0000

    New Project

---
+++ Project_6:_Celling_Fan/script.js
@@ -0,0 +1,25 @@
+speed= document.getElementsByClassName("fan");
+function on(){
+    speed[0].style.animationName="spin";
+}
+function off(){
+    speed[0].style.animationName="";
+}
+function speed1(){
+    speed[0].style.animationDuration="1s";
+}
+function speed2(){
+    speed[0].style.animationDuration="0.8s";
+}
+function speed3(){
+    speed[0].style.animationDuration="0.6s";
+}
+function speed4(){
+    speed[0].style.animationDuration="0.4s";
+}
+function speed5(){
+    speed[0].style.animationDuration="0.2s";
+}
+function speed6(){
+    speed[0].style.animationDuration="0.1s";
+}
